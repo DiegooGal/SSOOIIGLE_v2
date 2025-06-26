93cd95c1046ceb6d62cd923eba16833ff6d60813
DiegooGal <diego.garcia22@alu.uclm.es>
2025-06-26 16:39:08 +0200

    Update manager.cpp

--- Practica 3 - SSOOIIGLE/src/manager.cpp
+++ Practica 3 - SSOOIIGLE/src/manager.cpp
@@ -2,8 +2,7 @@
  * Práctica 2 - Sistemas Operativos II                                              *
  * SSOOIIGLE v2                                                                     *
  * Autores: Diego García Álvarez                                                    *
- *          Pablo Mollá GIl                                                         *
- * 10/05/2025                                                                       *
+ * 25/06/2025                                                                       *
  ************************************************************************************/
 
 // Inclusión de bibliotecas necesarias
@@ -18,16 +17,14 @@
 #include <processor.h>
 #include <definitions.h>
 #include <iomanip>
+#include <atomic>
 
 // Declaración de funciones
-void create_clients_thread();
-void create_dictionary();
-void signal_handler(int sig);
+void create_clients_threads();
 
 // Variables globales
-std::vector<std::thread> v_searching_threads;   // Vector de hilos de búsqueda
-std::vector<std::thread> v_client_threads;      // Vector de hilos de clientes
-std::unordered_set<std::string> dictionary_set;  // Set de diccionarios
+std::vector<std::thread> v_client_threads;
+std::atomic<bool> running = true;
 
 int main(){
     create_dictionary();
@@ -36,33 +33,29 @@ int main(){
     std::cout << "Número de procesadores disponibles: " << NUMBER_OF_THREADS << std::endl;
     std::cout << "-------------------------------------------------------------------------------------" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::thread payment_th(pay_system);
-    payment_th.detach();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    signal(SIGINT, signal_handler);
-    books_names();
-    create_clients_thread();
+    
+    while (running){
 
-    std::cout << "Todos los hilos terminados. Para salir -> Ctrl+C" << std::endl;
+    }
 
-    return EXIT_SUCCESS;
+    std::cout "\n [!] CTRL+C...";
+    return 0;
 }
+
 /****************************************************
 * Crea los hilos para alojar los clientes          *
 * @param .     No requerido                        *
 * @return      No devuelve nada                    *
 ***************************************************/
-void create_clients_thread(){
-    /* Calculamos un número aleatorio para el tipo de clientes y elegimos al azar la palabra entre las que hay en el diccionario,   *
-    *  con esto creamos los clientes y le asignamos su hilo correspondiente                                                         */
-    for(int i=0; i< NUMBER_OF_THREADS; i++){
+void create_clients_threads(){
+    for(int = 0; i<NUMBER_OF_THREADS){
         int type = rand() % 3;
         std::string random_word = *std::next(std::begin(dictionary_set), std::rand() % dictionary_set.size());
-
-        v_client_threads.push_back(std::thread(create_threads, i, type, random_word));
-        std::this_thread::sleep_for(std::chrono::seconds(1));   // Esperamos un segundo para no colapsar la terminal
+        
+        c_client_threads.push_back(std::thread(create_threads, i, type, random_word));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    /* Esperamos a la finalización de todos los clientes */
+
     std::for_each(v_client_threads.begin(), v_client_threads.end(), [](std::thread &t){t.join();});
 }
 
@@ -73,7 +66,7 @@ void create_clients_thread(){
 ***************************************************/
 void signal_handler(int num){
     std::cout << "\n[!] SIGINT signal received, killing processes and freeing resources." << std::endl;
-    exit(EXIT_SUCCESS);
+    running = false;
 }
 
 /****************************************************
@@ -83,15 +76,17 @@ void signal_handler(int num){
  ***************************************************/
 void create_dictionary(){
     std::ifstream file("docs/Diccionario.txt");
-    if (file.is_open()){
+    if (file.empty())
+    {
+        std::cout << "\n[!] El archivo no se pudo abrir o está vacío." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else if (file.is_open())
+    {
         std::string word;
-        while (file >> word){ 
+        while(file >> word){
             dictionary_set.insert(word);
         }
         file.close();
-    }
-    else{
-        std::cout << "Error: El archivo no existe o no se puede abrir." << std::endl;
-        exit(EXIT_FAILURE);
     }
 }
